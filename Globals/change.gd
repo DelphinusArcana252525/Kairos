0e91a0042e2db11260b5bd7811bede3da59c85d0
DelphinusArcana <zac.girash@gmail.com>
2025-05-07 12:51:22 -0400

    Adding the timeline to the world and adding to the platform tile map layer the code that interprets it. The world and timeline are currently on an example level.

--- Globals/change.gd
+++ Globals/change.gd
@@ -2,26 +2,16 @@ extends Node
 
 class_name Map_Change
 
-enum types {DELETE, ADD, MOVE}
+enum types {DELETE, ADD}
 
 const NO_INDEX = Vector2i(-1,-1)
 
 var type: types
-var add_pos: Vector2i
-var remove_pos: Vector2i
+var pos: Vector2i
 var tile_type: Vector2i
 
 # when moving, first_pos is add_pos and second_pos is remove_pos. When adding/deleting, only use first_pos
-func _init(type: types, first_pos: Vector2i = NO_INDEX, tile_type: Vector2i = NO_INDEX, second_pos: Vector2i = NO_INDEX) -> void:
+func _init(type: types, pos: Vector2i, tile_type: Vector2i = NO_INDEX) -> void:
 	self.type = type
 	self.tile_type = tile_type
-	match type :
-		types.DELETE: 
-			add_pos = NO_INDEX
-			remove_pos = first_pos
-		types.ADD:
-			add_pos = first_pos
-			remove_pos = NO_INDEX
-		types.MOVE:
-			add_pos = first_pos
-			remove_pos = second_pos
+	self.pos = pos
--- world.gd
+++ world.gd
@@ -1,19 +1,67 @@
 extends Node2D
 
-
-var changes: Array[Map_Change]
+var timeline: Array[Time_Event]
+var era: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	changes = [Map_Change.new(Map_Change.types.DELETE, Vector2i(5,8), Vector2i(0,1)), Map_Change.new(Map_Change.types.ADD, Vector2i(1,1))]
+	timeline = [
+		Time_Event.new([ # add the red platform
+			Map_Change.new(Map_Change.types.ADD, Vector2i(6,15), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.ADD, Vector2i(7,15), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.ADD, Vector2i(8,15), Vector2i(0,1))
+		], false, 1),
+		Time_Event.new([ # remove the red platform
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(6,15), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(7,15), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(8,15), Vector2i(0,1))
+		], false, 5),
+		Time_Event.new([ # remove the blue platforms
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(17,10), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(17,11), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(17,12), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(17,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(18,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(19,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(20,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(21,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(28,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(29,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(30,13), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.DELETE, Vector2i(31,13), Vector2i(0,1))
+		], false, 7),
+		Time_Event.new([ # add the yellow platforms
+			Map_Change.new(Map_Change.types.ADD, Vector2i(14,11), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.ADD, Vector2i(15,11), Vector2i(0,1)),
+			Map_Change.new(Map_Change.types.ADD, Vector2i(17,9), Vector2i(0,1))
+		], false, 7)
+	]
+	#timeline = [Time_Event.new([Map_Change.new(Map_Change.types.ADD, Vector2i(1,1), Vector2i(0,1))], false, 1),
+	#			Time_Event.new([Map_Change.new(Map_Change.types.ADD, Vector2i(1,2), Vector2i(0,1))], false, 5),
+	#			Time_Event.new([Map_Change.new(Map_Change.types.DELETE, Vector2i(7,12), Vector2i(0,1))], false, 3)]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("arbitrary_era"):
+		go_to_era(randi_range(0, 10))
 	if event.is_action_pressed("Next_Era"):
-		print("hi")
-		for change in changes:
-			print($Platforms.apply_change(change))
+		go_to_era(era + 1)
+	if event.is_action_pressed("reset"):
+		era = 0
+		print("reset")
+		$Platforms.reset_to_original()
+
+
+func go_to_era (era: int) -> void:
+	$Platforms.reset_to_original()
+	self.era = era
+	print("era " + str(era))
+	for time in range(era + 1): # +1 so it's inclusive
+			for time_event in timeline:
+				if time_event.time_to_happen == time:
+					$Platforms.apply_time_event(time_event)
