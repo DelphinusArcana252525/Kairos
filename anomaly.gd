bf00149d6e6ed2f288e141d5e8841549bda4964e
zyme-072 <alicezcat72@gmail.com>
2025-05-19 10:30:18 -0400

    anomaly movement
    he moves! chooses a random point and goes to it, is speedier when hostile, and can be reset to peaceful

--- anomaly.gd
+++ anomaly.gd
@@ -1,19 +1,57 @@
 extends StaticBody2D
 
 
-const SPEED = 300.0
+var speed = 2
 const JUMP_VELOCITY = -400.0
+var direct_vect
 var phase = 0
+var is_hostile : bool = false
+var goal_pos : Vector2 = Vector2(0,0)
 
 @export var hp : int
 
 #rewrite this so he doesnt move via keyboard
 #make him move randomly for now
 func _physics_process(delta: float) -> void:
+	if(is_goal_met(goal_pos) or goal_pos == Vector2(0,0)):
+		move_random()
+		print("changed goal")
+	self.position += direct_vect * speed
 	
 	pass
 
+func _ready() -> void:
+	move_random()
 	
 func take_damage(dmg) -> void:
 	hp -= dmg
+	is_hostile = true
 	print("Anomaly health: " + str(hp))
+	
+func move_random() -> void:
+	var bounds = get_viewport().get_visible_rect().size
+	#print(bounds)
+	var x_pos = randi_range(0, 1152)
+	var y_pos = randi_range(0,640)
+	var goal = Vector2(x_pos, y_pos)
+	get_direction(goal)
+	goal_pos = goal
+	if(is_hostile):
+		speed = 5
+
+	
+func get_direction(goal) -> void:
+	var direction : Vector2 = goal - self.position #?
+	var len = direction.length()
+	direction *= (1/len)
+	direct_vect = direction
+	#now we have a unit vector
+	
+func reset() -> void:
+	goal_pos = Vector2(0,0)
+	is_hostile = false
+	speed = 2
+
+func is_goal_met(goal) -> bool:
+	return abs(self.position.x - goal.x) < 5 and abs(self.position.y - goal.y) < 5
+	
